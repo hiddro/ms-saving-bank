94e740454517153e614efac422ae41c3a44b395f
@EduCCor <edd.ckalb@gmail.com>
2021-11-15 19:20:47 -0500

    saving account

---
+++ src/main/java/ms/saving/bank/MsSavingBankApplication.java
@@ -0,0 +1,13 @@
+package ms.saving.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSavingBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSavingBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ms/saving/bank/MsSavingBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.saving.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSavingBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
